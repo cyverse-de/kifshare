098d9040093856cfa3d17c34a8bdbfa9f83af6d3
Ian McEwen <mian@cyverse.org>
2016-12-05 17:38:17 -0700

    Add an application/json content type to JSON responses; this will ensure they get gzipped by nginx

--- src/kifshare/core.clj
+++ src/kifshare/core.clj
@@ -69,7 +69,7 @@
   (GET "/resources/js/:rsc-name"
        [rsc-name]
        (let [resource-root (ft/path-join (cfg/resources-root) (cfg/js-dir))]
-         (static-resp rsc-name :root resource-root)))
+         (resp/content-type (static-resp rsc-name :root resource-root) "application/json")))
 
   (GET "/resources/flash/:rsc-name"
        [rsc-name]
